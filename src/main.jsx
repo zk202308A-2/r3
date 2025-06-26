7615672f986d9b91dfc7cc54622c55a282fb2bc2
inhawinter <inhawinter@gmail.com>
2025-06-26 15:56:11 +0900

    aaaa

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,17 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router";
+import AboutPage from './pages/aboutPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello World</div>,
   },
+  {
+    path: "/about",
+    Component: AboutPage
+  }
 ]);
 
 createRoot(document.getElementById('root')).render(
