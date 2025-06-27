a8bce0450fc49bd4f9f1a75980f6bce3235bfda7
inhawinter <inhawinter@gmail.com>
2025-06-27 13:25:16 +0900

    aaa

--- src/apis/todoApi.jsx
+++ src/apis/todoApi.jsx
@@ -1,7 +1,12 @@
+import axios from "axios";
 
 export async function getTodoList(page) {
 
     //https://jsonplaceholder.typicode.com/todos
+
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/todos?_page=${page}`)
+
+    return res.data
 
 
 }
